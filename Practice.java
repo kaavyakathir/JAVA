456a736d17b5e6fa99e1eaed22bec1c0c60d4a99
Kaavya Kathirvel <kaavyakathir@gmail.com>
2026-08-22 15:59:59 +0530

    Delete Practice.java

--- Practice.java
+++
@@ -1,8 +0,0 @@
-public class Practice {
-    public static void main(String[] args) {
-        System.out.println("Hello, GitHub!");
-        
-        int a = 5, b = 3;
-        System.out.println("Sum: " + (a + b));
-    }
-}

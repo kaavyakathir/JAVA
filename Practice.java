dbce73491abed35b68d950b546f6d5cd574b4d40
kaavyakathir <kaavyakathir@gmail.com>
2026-08-22 13:25:39 +0530

    Entered practice code

---
+++ Practice.java
@@ -0,0 +1,8 @@
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+        
+        int a = 5, b = 3;
+        System.out.println("Sum: " + (a + b));
+    }
+}

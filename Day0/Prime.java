f0b179c634fbff296ab7dcdd09edd10d2ce9629b
kaavyakathir <kaavyakathir@gmail.com>
2026-09-19 08:50:27 +0530

    change folder

--- Day0/Prime.java
+++ Day0/Prime.java
@@ -1,3 +1,4 @@
+package Day0;
 import java.util.Scanner;
 public class Prime {
     public static void main(String[] args) {
--- Day0/Print.java
+++ Day0/Print.java
@@ -1,3 +1,4 @@
+package Day0;
 import java.util.Scanner;
 public class Print {
     public static void main(String[] args) {
